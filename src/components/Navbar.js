c46ff1c672c48752d091523ca6edce3545849eab
Jack R <thereapercountdown@outlook.com>
2022-11-10 14:59:32 +0000

    im gonna kill whoever made these languages istg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function NavbarHeader() {
   return (
@@ -12,22 +13,22 @@ function NavbarHeader() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="Programming" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/#/py">Python</NavDropdown.Item>
-              <NavDropdown.Item href="/#/c">C</NavDropdown.Item>
-              <NavDropdown.Item href="/#/hc">HTML / CSS</NavDropdown.Item>
+              <Link to="/py">Python</Link>
+              <Link to="/c">C</Link>
+              <Link to="/hc">HTML / CSS</Link>
             </NavDropdown>
             <NavDropdown title="Content Areas" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/#/ca1">CA1-4</NavDropdown.Item>
-              <NavDropdown.Item href="/#/ca2">CA5-8</NavDropdown.Item>
+              <Link to="/ca1">CA1-4</Link>
+              <Link to="/ca2">CA5-8</Link>
             </NavDropdown>
             <NavDropdown title="Projects" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/#/UIUX">UI/UX</NavDropdown.Item>
+              <Link to="/UIUX">UI/UX</Link>
             </NavDropdown>
-            <Nav.Link href="/#/placement">Industry Placement</Nav.Link>
-            <Nav.Link href="/#/event">Events</Nav.Link>
-            <Nav.Link href="/#/blog">Blog</Nav.Link>
-            <Nav.Link href="/#/masterclass">Masterclasses</Nav.Link>
-            <Nav.Link href="/#/contact">Contact</Nav.Link>
+            <Link to="/placement">Industry Placement</Link>
+            <Link to="/event">Events</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/masterclass">Masterclasses</Link>
+            <Link to="/contact">Contact</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
