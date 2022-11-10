6ced36caa5a2105ae53178cf12893cc784b7477d
Jack R <thereapercountdown@outlook.com>
2022-11-10 14:54:29 +0000

    powerpoint HAHA FUNNY

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,22 +12,22 @@ function NavbarHeader() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="Programming" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/py">Python</NavDropdown.Item>
-              <NavDropdown.Item href="/c">C</NavDropdown.Item>
-              <NavDropdown.Item href="/hc">HTML / CSS</NavDropdown.Item>
+              <NavDropdown.Item href="/#/py">Python</NavDropdown.Item>
+              <NavDropdown.Item href="/#/c">C</NavDropdown.Item>
+              <NavDropdown.Item href="/#/hc">HTML / CSS</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Content Areas" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/ca1">CA1-4</NavDropdown.Item>
-              <NavDropdown.Item href="/ca2">CA5-8</NavDropdown.Item>
+              <NavDropdown.Item href="/#/ca1">CA1-4</NavDropdown.Item>
+              <NavDropdown.Item href="/#/ca2">CA5-8</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Projects" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/UIUX">UI/UX</NavDropdown.Item>
+              <NavDropdown.Item href="/#/UIUX">UI/UX</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/placement">Industry Placement</Nav.Link>
-            <Nav.Link href="/event">Events</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
-            <Nav.Link href="/masterclass">Masterclasses</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/#/placement">Industry Placement</Nav.Link>
+            <Nav.Link href="/#/event">Events</Nav.Link>
+            <Nav.Link href="/#/blog">Blog</Nav.Link>
+            <Nav.Link href="/#/masterclass">Masterclasses</Nav.Link>
+            <Nav.Link href="/#/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,10 @@
 export default function Contactpage() {
-    return(
-        <p>a</p>
+    return (
+        <>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh', width: '100%', backgroundColor: '#0A2E36' }}>
+                <h1 id='title' style={{ color: '#14CC60', fontSize: '18px'}}>This page is simply to fulfil homework requirements, there is no way in hell i'm letting this page stay. If you want to know why then i'm more than happy to explain {'(Its not just because the slideshow is dumb)'}</h1>
+                <iframe src="https://farehamcollege-my.sharepoint.com/personal/jregan122_fareham_ac_uk/_layouts/15/Doc.aspx?sourcedoc={23eebbf9-898f-4154-91b8-23ee19725cbd}&amp;action=embedview&amp;wdAr=1.7777777777777777" style={{ height: '90vh', width: '60%', marginTop: '10px' }} frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+            </div>
+        </>
     );
 }
