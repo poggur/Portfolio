21208f82fed3487ac81449bbad32763af9750f37
Jack R <thereapercountdown@outlook.com>
2022-11-13 00:56:56 +0000

    improvements to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,32 +3,37 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function NavbarHeader() {
   return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: '#0A2E36'}}>
+    <Navbar collapseOnSelect expand="lg" variant="dark" style={{backgroundColor: '#0A2E36', display: 'flex', justifyContent: 'center'}}>
       <Container fluid>
-        <Navbar.Brand href="/" style={{color: '#14CC60'}}>Jack Regan</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Link to="/" style={{textDecoration: 'none'}}>
+            <h2 style={{color: '#14CC60'}}>Jack Regan</h2>
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavDropdown title="Programming" id="collasible-nav-dropdown">
-              <Link to="/py">Python</Link>
-              <Link to="/c">C</Link>
-              <Link to="/hc">HTML / CSS</Link>
+            <NavDropdown title="Programming" id="collasible-nav-dropdown" className='NavDrop'>
+              <Link className='NavbarLink' to="/py">Python</Link>
+              <Link className='NavbarLink' to="/c">C</Link>
+              <Link className='NavbarLink' to="/hc">HTML / CSS</Link>
             </NavDropdown>
             <NavDropdown title="Content Areas" id="collasible-nav-dropdown">
-              <Link to="/ca1">CA1-4</Link>
-              <Link to="/ca2">CA5-8</Link>
+              <Link className='NavbarLink' to="/ca1">CA1-4</Link>
+              <Link className='NavbarLink' to="/ca2">CA5-8</Link>
             </NavDropdown>
             <NavDropdown title="Projects" id="collasible-nav-dropdown">
-              <Link to="/UIUX">UI/UX</Link>
+              <Link className='NavbarLink' to="/UIUX">UI/UX</Link>
             </NavDropdown>
-            <Link to="/placement">Industry Placement</Link>
-            <Link to="/event">Events</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/masterclass">Masterclasses</Link>
-            <Link to="/contact">Contact</Link>
+            <Link className='NavbarLink' to="/placement">Industry Placement</Link>
+            <Link className='NavbarLink' to="/event">Events</Link>
+            <Link className='NavbarLink' to="/blog">Blog</Link>
+            <Link className='NavbarLink' to="/masterclass">Masterclasses</Link>
+            <Link className='NavbarLink' to="/contact">Contact</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
